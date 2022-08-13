1abbf90aafc03efcf1faa336c9ab22e3d3cc5f84
abhinandkr <abhinandkr1@gmail.com>
2022-08-13 20:37:06 +0530

    Removing PositionStack dependency * Using coordinates obtained from Wikidata

--- src/services/author-service.ts
+++ src/services/author-service.ts
@@ -1,25 +1,75 @@
 import {SPARQLQueryDispatcher} from '@shared/functions';
 
-async function getBirthPlace(name: string) {
-	const endpointUrl = 'https://query.wikidata.org/sparql';
-	const queriedLabel = 'birthLocationLabel';
-	const sparqlQuery = `SELECT DISTINCT ?${queriedLabel} WHERE {
+// class AuthorService {
+// 	private queryDispatcher: SPARQLQueryDispatcher;
+// 	constructor() {
+// 		const ENDPOINT_URL = 'https://query.wikidata.org/sparql';
+// 		this.queryDispatcher = new SPARQLQueryDispatcher(ENDPOINT_URL);
+// 	}
+// 	private async query(sparqlQuery: string) {
+// 		const res = await this.queryDispatcher.query(sparqlQuery);
+// 		return res;
+// 	}
+// }
+
+const ENDPOINT_URL = 'https://query.wikidata.org/sparql';
+
+async function getPlaceOfBirth(queryDispatcher: SPARQLQueryDispatcher, authorName: string) {
+	const birthLocation = 'birthLocation';
+	const birthLocationLabel = 'birthLocationLabel';
+	const sparqlQuery = `SELECT DISTINCT ?${birthLocation} ?${birthLocationLabel} WHERE {
   ?item ((wdt:P31|wdt:P101|wdt:P106)/(wdt:P279*)) wd:Q482980;
-    rdfs:label "${name}"@en;
+    rdfs:label "${authorName}"@en;
     wdt:P19 ?birthLocation.
   SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
 }`;
 
-	const queryDispatcher = new SPARQLQueryDispatcher(endpointUrl);
-	try {
-		const res = await queryDispatcher.query(sparqlQuery);
-		if (res === null) {
-			return null;
-		}
-		return res[queriedLabel].value;
-	} catch (e) {
-		throw e;
+	const res = await queryDispatcher.query(sparqlQuery);
+	if (res === null) {
+		return null;
 	}
+	return {
+		birthLocation: res[birthLocation].value.split('/').pop(),
+		birthLocationLabel: res[birthLocationLabel].value,
+	};
+}
+
+
+async function getCountryLatLong(queryDispatcher: SPARQLQueryDispatcher, birthLocation: string) {
+	const countryLabel = 'countryLabel';
+	const longitude = 'longitude';
+	const latitude = 'latitude';
+	const sparqlQuery = `SELECT ?${countryLabel} ?${longitude} ?${latitude} WHERE {
+  wd:${birthLocation} p:P625 ?coordinate.
+  ?coordinate psv:P625 ?coordinate_node.
+  ?coordinate_node wikibase:geoLongitude ?${longitude};wikibase:geoLatitude ?${latitude}.
+  wd:${birthLocation} wdt:P17 ?country.
+  ?country rdfs:label ?${countryLabel}.
+  FILTER((LANG(?${countryLabel})) = "en")
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+}`;
+	const res = await queryDispatcher.query(sparqlQuery);
+	if (res === null) {
+		return null;
+	}
+	return {
+		countryLabel: res[countryLabel].value,
+		longitude: res[longitude].value,
+		latitude: res[latitude].value,
+	};
+}
+
+async function getBirthPlace(name: string) {
+	const queryDispatcher = new SPARQLQueryDispatcher(ENDPOINT_URL);
+	const birthObj = await getPlaceOfBirth(queryDispatcher, name);
+	if (!birthObj) {
+		return null;
+	}
+	const birthPlaceObj = await getCountryLatLong(queryDispatcher, birthObj?.birthLocation);
+	return {
+		birthLocationLabel: birthObj.birthLocationLabel,
+		...birthPlaceObj
+	};
 }
 
 export default {
